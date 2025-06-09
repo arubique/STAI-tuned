400cccf33b4b306ff9ace580d68a97707cd058ca
Alexander Rubinstein <rubalex14@gmail.com>
2025-06-09 12:15:18 +0200

    Remove functions that are already in stnd

--- src/stuned/run_from_csv/__init__.py
+++ src/stuned/run_from_csv/__init__.py
@@ -1,5 +1,5 @@
-from .__main__ import (
+from stnd.run_from_csv import (
     main,
     make_final_cmd_slurm,
-    extract_from_csv_row_by_prefix
+    extract_from_csv_row_by_prefix,
 )
---
+++ src/stuned/utility/helpers_for_main/__init__.py
@@ -0,0 +1,8 @@
+from stnd.utility.helpers_for_main import (
+    parse_args,
+    get_diff_with_unstaged_changes,
+    prepare_wrapper_for_experiment,
+    define_env_vars,
+    SCRATCH_LOCAL,
+    SCRATCH_VAR_NAME,
+)

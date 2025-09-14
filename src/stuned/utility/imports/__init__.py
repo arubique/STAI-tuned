f56d7f7b33a6be66f8a231e294df8c7eb17bfb53
Alexander Rubinstein <rubalex14@gmail.com>
2025-09-14 12:57:37 +0200

    Move import utils to stnd

---
+++ src/stuned/utility/imports/__init__.py
@@ -0,0 +1,14 @@
+from stnd.utility.imports import (
+    PACKAGE_SEPARATOR,
+    FROM_CLASS_KEY,
+    LazyModuleWrapper,
+    is_bulitin_name,
+    importlib_lazy_import,
+    lazy_import,
+    _LazyModule,
+    make_lazy_module,
+    pop_all_modules_by_filter,
+    import_from_string,
+    make_from_class_ctor,
+    update_enums_in_config,
+)
--- tests/test_main.py
+++ tests/test_main.py
@@ -349,5 +349,19 @@ def test_imports_for_moved_to_stnd():
         download_and_extract_tar,
         optionally_make_dir,
     )
+    from stuned.utility.imports import (
+        PACKAGE_SEPARATOR,
+        FROM_CLASS_KEY,
+        LazyModuleWrapper,
+        is_bulitin_name,
+        importlib_lazy_import,
+        lazy_import,
+        _LazyModule,
+        make_lazy_module,
+        pop_all_modules_by_filter,
+        import_from_string,
+        make_from_class_ctor,
+        update_enums_in_config,
+    )
 
     pass
